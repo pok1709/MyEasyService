ae0b4f61516db1723072e2fec4e14a11583cd9f1
pok1709 <pok1709@gmail.com>
2017-10-21 15:59:05 +0700

    Create Main Fragment Class Success

--- app/src/main/java/app/cpt/sorapongph/myeasyservice/MainActivity.java
+++ app/src/main/java/app/cpt/sorapongph/myeasyservice/MainActivity.java
@@ -3,11 +3,20 @@ package app.cpt.sorapongph.myeasyservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.cpt.sorapongph.myeasyservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        if (savedInstanceState == null) {
+            // Load Fragment View
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFrangmentMain, new MainFragment()).commit();
+        }
+    }   // Main Method
+
+}   // Main Class
