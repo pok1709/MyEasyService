395fa81b8a04e3ca189d360bb08f97666c9d9181
pok1709 <pok1709@gmail.com>
2017-10-22 11:06:45 +0700

    ฺBackController Success

--- app/src/main/java/app/cpt/sorapongph/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/app/cpt/sorapongph/myeasyservice/fragment/SecondFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import app.cpt.sorapongph.myeasyservice.R;
 
@@ -15,9 +16,32 @@ import app.cpt.sorapongph.myeasyservice.R;
 
 public class SecondFragment extends Fragment {
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Back Controller
+        backController();
+
+    }// Main Method
+
+    private void backController() {
+        Button button = getView().findViewById(R.id.btnBack);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //สลายหน้ากากตัวเอง แล้วกลับไปยังหน้ากากที่เปิดก่อนหน้า
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
         return view;
     }
