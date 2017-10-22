8d26c1c340af014e6ead36b030d5a61e322ba3ec
pok1709 <pok1709@gmail.com>
2017-10-22 14:44:53 +0700

    Hemberger Icon Item Success

--- app/src/main/java/app/cpt/sorapongph/myeasyservice/MainActivity.java
+++ app/src/main/java/app/cpt/sorapongph/myeasyservice/MainActivity.java
@@ -1,16 +1,24 @@
 package app.cpt.sorapongph.myeasyservice;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import app.cpt.sorapongph.myeasyservice.fragment.MainFragment;
+import app.cpt.sorapongph.myeasyservice.fragment.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,48 @@ public class MainActivity extends AppCompatActivity {
         //Text Controller
         textController();
 
+        //Create Toolbar
+        createToolbar();
+
     }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    private void createToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolBarMain);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+        );
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
     private void textController() {
         TextView mainTextView = (TextView) findViewById(R.id.txtMainFragment);
@@ -37,6 +86,9 @@ public class MainActivity extends AppCompatActivity {
         mainTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFrangmentMain, new MainFragment())
+                        .commit();
                 //Close Drawer
                 drawerLayout.closeDrawers();
             }
@@ -46,6 +98,9 @@ public class MainActivity extends AppCompatActivity {
         secondTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFrangmentMain, new SecondFragment())
+                        .commit();
                 drawerLayout.closeDrawers();
             }
         });
@@ -54,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         exitTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
                 drawerLayout.closeDrawers();
             }
         });
