8826506138585d351a0ea3a9518238a66669047b
pok1709 <pok1709@gmail.com>
2017-10-22 13:51:16 +0700

    Drawer Close Menu Success

--- app/src/main/java/app/cpt/sorapongph/myeasyservice/MainActivity.java
+++ app/src/main/java/app/cpt/sorapongph/myeasyservice/MainActivity.java
@@ -1,22 +1,75 @@
 package app.cpt.sorapongph.myeasyservice;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import app.cpt.sorapongph.myeasyservice.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Add Fragment to Activity
+        addFragment(savedInstanceState);
+
+        //Setup Drawer Menu
+        setupDrawerMenu();
+
+        //Text Controller
+        textController();
+
+    }   // Main Method
+
+    private void textController() {
+        TextView mainTextView = (TextView) findViewById(R.id.txtMainFragment);
+        TextView secondTextView = (TextView) findViewById(R.id.txtSecondFragment);
+        TextView exitTextView = (TextView) findViewById(R.id.txtExit);
+
+        //For MainFragment
+        mainTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Close Drawer
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        //For SecondFragment
+        secondTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        //For ExitFragment
+        exitTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+    }
+
+    private void setupDrawerMenu() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.myDrawerLayout);
+    }
+
+    private void addFragment(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             // Load Fragment View
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentFrangmentMain, new MainFragment()).commit();
         }
-    }   // Main Method
+    }
 
 }   // Main Class
